ab43da3ba51e5c30fbe52f2f083ae04c44d77e24
Mizrahi, Avshalom <avshalom.mizrahi@intel.com>
2024-10-31 06:52:03 -0700

    basic staff is working

--- lua/jir_calc/command.lua
+++ lua/jir_calc/command.lua
@@ -1,41 +1,43 @@
-local log = require("jir_calc.log")
-
 local M = {}
 
 local function is_empty_or_spaces(str)
 	return str:match("^%s*$") ~= nil
 end
 
+local function expr_prep(calc_expr)
+    local value = calc_expr
+    return value
+end
+
 -- Function to handle user commands
 function M.handle_command(main_win)
 	local cmd_buf = vim.api.nvim_get_current_buf()
 	local expr = vim.api.nvim_buf_get_lines(cmd_buf, 0, -1, false)[1]
+	local output_string
 
 	-- Strip the leading "> " from the expression
 	expr = expr:sub(3)
 
-	local result, err = load("return " .. expr)
 	if is_empty_or_spaces(expr) then
-		vim.api.nvim_err_writeln("Error: Empty expression")
-		log.log("Error: Empty expression")
-	else
-		if err then
-			vim.api.nvim_err_writeln("Error: " .. err)
-			log.log("Error: " .. err)
-		else
-			local success, value = pcall(result)
-			if success then
-				vim.api.nvim_out_write("" .. expr .. value .. "\n")
-				log.log("Result: " .. value)
-				-- Update the main window buffer with the result
-				local main_buf = vim.api.nvim_win_get_buf(main_win)
-				vim.api.nvim_buf_set_lines(main_buf, -1, -1, false, { "Result: " .. value })
-			else
-				vim.api.nvim_err_writeln("Error: " .. value)
-				log.log("Error: " .. value)
-			end
-		end
-	end
+        vim.api.nvim_err_writeln("Error: Empty expression")
+    else
+        local temp_expr = expr_prep(expr)
+        local result_expr, err = load("return " .. temp_expr)
+        if err then
+            vim.api.nvim_err_writeln("Error: " .. err)
+        else
+            local success, value = pcall(result_expr)
+            if success then
+                local main_buf = vim.api.nvim_win_get_buf(main_win)
+                local line_count = vim.api.nvim_buf_line_count(main_buf)
+                output_string = expr .. " = " .. value
+                vim.api.nvim_buf_set_lines(main_buf, line_count, line_count, false, { output_string })
+                vim.api.nvim_win_set_cursor(main_win, { line_count + 1, 0 })
+            else
+                vim.api.nvim_err_writeln("Error: " .. value)
+            end
+        end
+    end
 
 	-- Clear the command buffer and keep it in insert mode
 	vim.api.nvim_buf_set_lines(cmd_buf, 0, -1, false, { "> " })
