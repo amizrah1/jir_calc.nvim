6a6e44d6f8d4f91d2265079d7bdc48f6585477fd
Mizrahi, Avshalom <avshalom.mizrahi@intel.com>
2024-10-31 07:34:19 -0700

    basic staff is working

--- lua/jir_calc/command.lua
+++ lua/jir_calc/command.lua
@@ -1,7 +1,8 @@
 local M = {}
+local expr_prep_module = require('jir_calc.expr_prep')
 
 local function is_empty_or_spaces(str)
-	return str:match("^%s*$") ~= nil
+    return str:match("^%s*$") ~= nil
 end
 
 local function expr_prep(calc_expr)
@@ -11,17 +12,17 @@ end
 
 -- Function to handle user commands
 function M.handle_command(main_win)
-	local cmd_buf = vim.api.nvim_get_current_buf()
-	local expr = vim.api.nvim_buf_get_lines(cmd_buf, 0, -1, false)[1]
-	local output_string
+    local cmd_buf = vim.api.nvim_get_current_buf()
+    local expr = vim.api.nvim_buf_get_lines(cmd_buf, 0, -1, false)[1]
+    local output_string
 
-	-- Strip the leading "> " from the expression
-	expr = expr:sub(3)
+    -- Strip the leading "> " from the expression
+    expr = expr:sub(3)
 
-	if is_empty_or_spaces(expr) then
+    if is_empty_or_spaces(expr) then
         vim.api.nvim_err_writeln("Error: Empty expression")
     else
-        local temp_expr = expr_prep(expr)
+        local temp_expr = expr_prep_module.expr_prep(expr)
         local result_expr, err = load("return " .. temp_expr)
         if err then
             vim.api.nvim_err_writeln("Error: " .. err)
@@ -39,10 +40,10 @@ function M.handle_command(main_win)
         end
     end
 
-	-- Clear the command buffer and keep it in insert mode
-	vim.api.nvim_buf_set_lines(cmd_buf, 0, -1, false, { "> " })
-	vim.api.nvim_command("startinsert")
-	vim.api.nvim_win_set_cursor(0, { 1, 3 }) -- Position cursor after "> "
+    -- Clear the command buffer and keep it in insert mode
+    vim.api.nvim_buf_set_lines(cmd_buf, 0, -1, false, { "> " })
+    vim.api.nvim_command("startinsert")
+    vim.api.nvim_win_set_cursor(0, { 1, 3 }) -- Position cursor after "> "
 end
 
 return M
---
+++ lua/jir_calc/expr_prep.lua
@@ -0,0 +1,9 @@
+local M = {}
+
+function M.expr_prep(calc_expr)
+    local value = calc_expr
+    return value
+end
+
+return M
+
